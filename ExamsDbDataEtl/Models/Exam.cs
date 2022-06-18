481645f2a7ff91a6405c885db9dcaa7f923d5c2c
amalkrishna <amalkrishna263@outlook.com>
2022-06-18 11:25:51 +0530

    Add DB context and models

---
+++ ExamsDbDataEtl/Models/Exam.cs
@@ -0,0 +1,18 @@
+﻿namespace ExamsDbDataEtl.Models;
+
+public partial class Exam
+{
+    public Exam()
+    {
+        ExamQuestions = new HashSet<ExamQuestion>();
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public int? CourseId { get; set; }
+    public DateTime StartTime { get; set; }
+    public TimeSpan Duration { get; set; }
+
+    public virtual Course? Course { get; set; }
+    public virtual ICollection<ExamQuestion> ExamQuestions { get; set; }
+}
---
+++ ExamsDbDataEtl/Models/ExamQuestion.cs
@@ -0,0 +1,17 @@
+﻿namespace ExamsDbDataEtl.Models;
+
+public partial class ExamQuestion
+{
+    public ExamQuestion()
+    {
+        ExamQuestionResponses = new HashSet<ExamQuestionResponse>();
+    }
+
+    public int Id { get; set; }
+    public int? ExamId { get; set; }
+    public int? QuestionId { get; set; }
+
+    public virtual Exam? Exam { get; set; }
+    public virtual Question? Question { get; set; }
+    public virtual ICollection<ExamQuestionResponse> ExamQuestionResponses { get; set; }
+}
